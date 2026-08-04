64115e4674455d3df02708b504062357a6ca28cb
Haimanot Beka Mekonnen <haimanotbeka@gmail.com>
2026-08-04 11:32:41 +0300

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import API from "../api/api.jsx";
-import "./login.css";
+import "./Login.css";
 
 const Login = ({ setLoggedIn, setIsAdmin, closeModal, switchToRegister }) => {
   const { t } = useTranslation();
